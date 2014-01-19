a0d98f55cea914bce39016a5826e7b09e8a59c7f
Scott Walters <scott@slowass.net>
2014-01-19 15:41:11 -0600

    fix up 04_runtime.pl to use a table of actual 6502 instruction cycle counts, 6502_formatted.txt. all of the times listed are best case (don't have additional cycles added in for if branches are taken or page boundaries are crossed). I didn't preserved all of the additional cycle notes (3rd column) either. 04_runtime.pl doesn't actually have any tests in it yet; it just generates informational output. here's the output: supposedly, there are 5320 machine cycles total available during vblank and overscan 7813 7708 5758 7238 6296 ... so, all of the different perspectives take more time to render than we have available. newbies.asm is currently rolling the screen when deltay = 0 and deltaz = 0 facing a platform. the first two numbers reflect that, but the numbers aren't so high that no INTM value should be able to prevent the screen roll unless I guess it takes so long to render that first platform that even #96 on the timer isn't enough. need more tests.

---
+++ 04_runtime.pl
@@ -0,0 +1,129 @@
+#!/usr/local/bin/perl
+
+# make sure various bits of logic do the right thing with deltaz/deltay
+
+use strict;
+use warnings;
+
+use Test::More;
+
+use Acme::6502;
+
+use lib '.';
+use symbols;
+
+use Data::Dumper;
+use Carp; $SIG{__DIE__} = sub { Carp::confess @_ };
+
+use PadWalker;
+
+my $symbols = symbols::symbols('newbies.lst');
+
+my $cpu = Acme::6502->new();
+$cpu->load_rom( 'newbies.bin', 0xf000 );
+
+my $cycles_per_opcode;
+open my $fh, '6502_formatted.txt' or die $!;
+while( my $line = readline $fh ) {
+    chomp $line;
+    my @line = split m/ /, $line;
+    @line >= 2 or die $line;
+    # warn "$line[0] = $line[1]\n";
+    $cycles_per_opcode->[ hex($line[0]) ] = $line[1];
+}
+
+sub run_cpu {
+    my @stop_symbols = @_;
+    my $cycles = 0;
+    $cpu->run(10000, sub {
+            my ($pc, $inst, $a, $x, $y, $s, $p) = @_;
+            # diag sprintf "pc = %x inst = %x a = %s x = %s y = %x", $pc, $inst, $a, $x, $y;
+            $cycles_per_opcode->[$inst] or die sprintf( "%2x (%d) has no cycle count", $inst, $inst) . "\n" . Dumper( $cycles_per_opcode );
+            $cycles += $cycles_per_opcode->[$inst];
+            if( grep $pc == $_, @stop_symbols ) {
+                ${ PadWalker::peek_my(1)->{'$ic'} } = 0;
+            }
+    });
+    return $cycles;
+}
+
+#
+# check how long it takes to render different perspectives
+#
+
+# init
+
+my $level0 = $symbols->level0 or die;
+for my $sym (
+        1, 11, 0x1e,  0xe0,
+        20, 25, 0x14, 0x60,
+        30, 40, 0x18, 0x20,
+        0, 0, 0, 0,
+) {
+    $cpu->write_8( $level0++, $sym );
+}
+
+$symbols->INTIM or die;
+$cpu->write_8( $symbols->INTIM, 30 );
+
+my $cycles;
+
+#
+# tests
+#
+
+diag "supposedly, there are 5320 machine cycles total available during vblank and overscan";
+
+# here's a troublesome one:
+
+$cpu->set_pc( $symbols->platlevelclear );
+$cpu->write_8( $symbols->playerz, 0x00 );
+$cpu->write_8( $symbols->playery, 0x20 );
+ 
+$cycles = run_cpu( $symbols->vblanktimerendalmost );
+
+diag $cycles;  # not really cycles; just an instruction count; currently 2807 ... yeah, that's a lot
+
+# and another troublesome one:
+
+$cpu->set_pc( $symbols->platlevelclear );
+$cpu->write_8( $symbols->playerz, 0x02 );
+$cpu->write_8( $symbols->playery, 0x1d );
+ 
+$cycles = run_cpu( $symbols->vblanktimerendalmost );
+
+diag $cycles;  # 2773
+
+
+# and a made up one
+
+$cpu->set_pc( $symbols->platlevelclear );
+$cpu->write_8( $symbols->playerz, 0x00 );
+$cpu->write_8( $symbols->playery, 0x10 );
+ 
+$cycles = run_cpu( $symbols->vblanktimerendalmost );
+
+diag $cycles;  # 2113
+
+# starting view (looking out over a platform, with $39 = 57 bits of gap filled)
+
+$cpu->set_pc( $symbols->platlevelclear );
+$cpu->write_8( $symbols->playerz, 0x02 );
+$cpu->write_8( $symbols->playery, 0x20 );
+ 
+$cycles = run_cpu( $symbols->vblanktimerendalmost );
+
+diag $cycles;  # 2591
+
+# a view with only $0f gaps filled
+
+$cpu->set_pc( $symbols->platlevelclear );
+$cpu->write_8( $symbols->playerz, 0x02 );
+$cpu->write_8( $symbols->playery, 0x18 );
+ 
+$cycles = run_cpu( $symbols->vblanktimerendalmost );
+
+diag $cycles;  # 2267
+
+
+done_testing();
