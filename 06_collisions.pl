7812b095d6417c8dcb99c80ace31620133fb9dfd
Scott Walters <scott@slowass.net>
2014-01-19 22:30:08 -0600

    start of collision detecting logic (including figuring out if we're standing on something), including unit tests in 06_collisions.pl

---
+++ 06_collisions.pl
@@ -0,0 +1,78 @@
+#!/usr/local/bin/perl
+
+# test collision detection logic
+
+use strict;
+use warnings;
+
+use Test::More;
+
+use Acme::6502;
+
+use lib '.';
+use symbols;
+
+use Data::Dumper;
+use Carp; $SIG{__DIE__} = sub { Carp::confess @_ };
+
+use PadWalker;
+
+my $symbols = symbols::symbols('newbies.lst');
+
+my $cpu = Acme::6502->new();
+$cpu->load_rom( 'newbies.bin', 0xf000 );
+
+sub run_cpu {
+    my @stop_symbols = @_;
+    my $cycles = 0;
+    $cpu->run(10000, sub {
+        my ($pc, $inst, $a, $x, $y, $s, $p) = @_;
+        my $name = name_that_location($pc);
+        # diag $name if $name !~ m/unknown/;
+        # diag sprintf "pc = %x inst = %x a = %s x = %s y = %x", $pc, $inst, $a, $x, $y;
+        if( grep $pc == $_, @stop_symbols ) {
+            ${ PadWalker::peek_my(1)->{'$ic'} } = 0;
+        }
+    });
+    return $cycles;
+}
+
+sub name_that_location {
+    my $loc = shift;
+    my %locations = reverse %$symbols;
+    return $locations{$loc} if $locations{$loc};
+    return $locations{$loc-1} if $locations{$loc-1};
+    return $locations{$loc-2} if $locations{$loc-2};
+    return $locations{$loc-3} if $locations{$loc-3};
+    return 'unknown location';
+}
+
+
+#
+# init
+#
+
+my $level0 = $symbols->level0 or die;
+for my $sym (
+        1, 11, 0x1e,  0xe0,
+        20, 25, 0x14, 0x60,
+        30, 40, 0x18, 0x20,
+        0, 0, 0, 0,
+) {
+    $cpu->write_8( $level0++, $sym );
+}
+
+# test standing on a platform
+
+$cpu->set_pc( $symbols->readstick ); # XXX subject to change
+$cpu->write_8( $symbols->playerz, 20 );
+$cpu->write_8( $symbols->playery, 0x14+1 );
+ 
+run_cpu( $symbols->collisions1a, $symbols->collisions9 );
+
+my $loc = name_that_location( $cpu->get_pc );
+is $loc, 'collisions1a', 'stopped at collisions1a, where tmp2 is written, not collisions9, where it gave up and existed';
+
+is $cpu->read_8( $symbols->tmp2 ), 4, 'collision logic decided that we are standing on the second platform which has index 4';
+
+done_testing();
--- newbies.asm
+++ newbies.asm
@@ -214,13 +214,11 @@ flap_y		= %01111111;
 
 		stx tmp1				; hold the new lastline here until after we're done recursing to fill in the gaps; only do this when we're first called, not when we recurse
 
-
-; XXX fetch the current platform color and stash it in tmp2 then put tmp1 back into X
+		; fetch the current platform color and stash it in tmp2 then put tmp1 back into X
 		ldx curplat
 		lda level0+3,x
 		sta tmp2
 		ldx tmp1
-
 
 .plotonscreen2
 		lda view,x				; get the line width of what's there already
@@ -611,9 +609,7 @@ platrenderline1
 		tay						; Y gets the distance, fresh back from plathypot, which we use to figure out which size of line to draw
 		tsx						; X gets the scanline to draw at; value for curlineoffset is hidden in the S register
 
-do_plotonscreen
 		_plotonscreen			; Y gets the distance away/platform line width, X gets the scanline to draw at
-		; fall through to platnextline
 
 platnextline
 
@@ -634,10 +630,9 @@ platnextline0
 
 		; deltaz = 0 is okay; stop on deltaz < 0
 		bmi platnextline3		; on deltaz < 1, branch to platnext to start in on the next platform; we've walked backwards past the players position for this platform
-		lda deltaz
-
 		; for platforms that start in front of us, only draw them back to the point where they start
 		; if the start of the platform is behind us, then the deltaz check above will catch that so don't worry about that case
+		lda deltaz
 		ldy curplat
 		lda level0,y			; don't take deltaz below level0+0,y - playerz; aka, stop when deltaz reaches the start of the platform
 		sec
@@ -691,6 +686,50 @@ vblanktimerendalmost2
 ;
 
 readstick
+
+		; detect collisions XXXXXXXXXXXXX
+
+		ldy #$ff
+		sty tmp2				; use tmp2 to record which, if any, platform we're standing on
+		ldy #0
+		sty tmp1				; use tmp1 for collision bits
+		; sty deltaz	; don't think we need this
+		; sty curplat	; just keep it in Y?
+collisions1
+		lda level0,y			; load the first byte, the Z start position, of the current platform
+		beq collisions9			; stop when there's no more platform data
+
+		; are we standing on this platform?
+		lda playery
+		clc						; our feet are one unit above our head, I guess, so subtract one extra
+		sbc level0+2,y			; subtract the 3rd byte, the platform height
+		bne collisions2
+		; make sure we're >= the start of it and <= the end of it
+		lda playerz
+		cmp level0+0,y
+		bmi collisions2			; if playerz - platstart < 0, the platform hasn't started yet
+		lda level0+1,y
+		cmp playerz
+		bmi	collisions2			; if platend - playerz < 0, we're off the end of the platform
+		; okay, we're standing on this platform!
+collisions1a
+		sty tmp2
+		; XX maybe jmp out?  or are we going to start checking for other kinds of collisions?
+; XXXX should probably have done the z checks first and then we could both check if we're standing on the platform or if we've hit our head
+		nop
+
+collisions2
+		; try another platform?
+		iny
+		iny
+		iny
+		iny
+		; sty curplat
+		jmp collisions1
+
+collisions9
+
+
 		; bit 3 = right, bit 2 = left, bit 1 = down, bit 0 = up, one stick per nibble
 		lda SWCHA
 		and #$f0
@@ -1120,54 +1159,45 @@ arctangent
 
 platformcolors
 
-; in the framebuffer, the top three bits are the color and the bottom five bits (but only up to dec 30) are the line width;
+; in the framebuffer, the top three bits are the color and the bottom five bits (but only up to dec 29) are the line width;
 ; this is a quick translation to the top four bits being color and the bottom four being the scaled brightness (based on line width which implies distance)
+; 8 lines with 32 entries on each (but only the first 29 items on each line will ever be used)
+; we want a log drop off
+; not quite right; still dicking with this
 
-; for my $i (0..255) {
-;     my $color = $i & 0b11100000;
-;     my $dist =  $i & 0b00011111;
-;     $dist = int( 0x0f / 30 * $dist );
-;     print "\t\t.byte " if 0 == ( $i & 0b0111 );
-;     printf "%%%08b", $color | $dist, "\n";
-;     print( (  0b0111 == ( $i & 0b0111 ) ) ? "\n" : ', ' );
+
+; use strict;
+; use warnings;
+; use Math::Trig;
+; # 30 datapoints (0-29)
+; # each in the range of 0-15
+; # so convert 0-29 to 0-90 and then scale that to 0-15
+; my @brightnesses;
+; for(my $i = 0; $i < 32; $i++ ) {
+;     my $width = $i > 29 ? 29 : $i;
+;     # push @brightnesses, int( rad2deg( cos( deg2rad($stop) ) ) );
+;     my $multiplier = 29 / sqrt(29);
+;     push @brightnesses, $multiplier * sqrt $width;
+; }
+; warn "brightnesses: @brightnesses\n";
+; @brightnesses = reverse @brightnesses;
+; for my $b (@brightnesses ) {
+;     print 'X' x $b , "\n";
+; }
+; for( my $color = 0; $color <= 0b11100000; $color += 0b00100000 ) {
+;     print "\t\t.byte ";
+;     print join ', ', map { sprintf "%%%08b", $_ } map { $color | int($_) } @brightnesses;
+;     print "\n";
 ; }
 
-                .byte %00000000, %00000000, %00000001, %00000001, %00000010, %00000010, %00000011, %00000011
-                .byte %00000100, %00000100, %00000101, %00000101, %00000110, %00000110, %00000111, %00000111
-                .byte %00001000, %00001000, %00001001, %00001001, %00001010, %00001010, %00001011, %00001011
-                .byte %00001100, %00001100, %00001101, %00001101, %00001110, %00001110, %00001111, %00001111
-                .byte %00100000, %00100000, %00100001, %00100001, %00100010, %00100010, %00100011, %00100011
-                .byte %00100100, %00100100, %00100101, %00100101, %00100110, %00100110, %00100111, %00100111
-                .byte %00101000, %00101000, %00101001, %00101001, %00101010, %00101010, %00101011, %00101011
-                .byte %00101100, %00101100, %00101101, %00101101, %00101110, %00101110, %00101111, %00101111
-                .byte %01000000, %01000000, %01000001, %01000001, %01000010, %01000010, %01000011, %01000011
-                .byte %01000100, %01000100, %01000101, %01000101, %01000110, %01000110, %01000111, %01000111
-                .byte %01001000, %01001000, %01001001, %01001001, %01001010, %01001010, %01001011, %01001011
-                .byte %01001100, %01001100, %01001101, %01001101, %01001110, %01001110, %01001111, %01001111
-                .byte %01100000, %01100000, %01100001, %01100001, %01100010, %01100010, %01100011, %01100011
-                .byte %01100100, %01100100, %01100101, %01100101, %01100110, %01100110, %01100111, %01100111
-                .byte %01101000, %01101000, %01101001, %01101001, %01101010, %01101010, %01101011, %01101011
-                .byte %01101100, %01101100, %01101101, %01101101, %01101110, %01101110, %01101111, %01101111
-                .byte %10000000, %10000000, %10000001, %10000001, %10000010, %10000010, %10000011, %10000011
-                .byte %10000100, %10000100, %10000101, %10000101, %10000110, %10000110, %10000111, %10000111
-                .byte %10001000, %10001000, %10001001, %10001001, %10001010, %10001010, %10001011, %10001011
-                .byte %10001100, %10001100, %10001101, %10001101, %10001110, %10001110, %10001111, %10001111
-                .byte %10100000, %10100000, %10100001, %10100001, %10100010, %10100010, %10100011, %10100011
-                .byte %10100100, %10100100, %10100101, %10100101, %10100110, %10100110, %10100111, %10100111
-                .byte %10101000, %10101000, %10101001, %10101001, %10101010, %10101010, %10101011, %10101011
-                .byte %10101100, %10101100, %10101101, %10101101, %10101110, %10101110, %10101111, %10101111
-                .byte %11000000, %11000000, %11000001, %11000001, %11000010, %11000010, %11000011, %11000011
-                .byte %11000100, %11000100, %11000101, %11000101, %11000110, %11000110, %11000111, %11000111
-                .byte %11001000, %11001000, %11001001, %11001001, %11001010, %11001010, %11001011, %11001011
-                .byte %11001100, %11001100, %11001101, %11001101, %11001110, %11001110, %11001111, %11001111
-                .byte %11100000, %11100000, %11100001, %11100001, %11100010, %11100010, %11100011, %11100011
-                .byte %11100100, %11100100, %11100101, %11100101, %11100110, %11100110, %11100111, %11100111
-                .byte %11101000, %11101000, %11101001, %11101001, %11101010, %11101010, %11101011, %11101011
-                .byte %11101100, %11101100, %11101101, %11101101, %11101110, %11101110, %11101111, %11101111
-
-;
-; background
-;
+		.byte %00011101, %00011101, %00011101, %00011100, %00011011, %00011011, %00011010, %00011010, %00011001, %00011001, %00011000, %00011000, %00010111, %00010110, %00010110, %00010101, %00010100, %00010100, %00010011, %00010010, %00010001, %00010001, %00010000, %00001111, %00001110, %00001101, %00001100, %00001010, %00001001, %00000111, %00000101, %00000000
+		.byte %00111101, %00111101, %00111101, %00111100, %00111011, %00111011, %00111010, %00111010, %00111001, %00111001, %00111000, %00111000, %00110111, %00110110, %00110110, %00110101, %00110100, %00110100, %00110011, %00110010, %00110001, %00110001, %00110000, %00101111, %00101110, %00101101, %00101100, %00101010, %00101001, %00100111, %00100101, %00100000
+		.byte %01011101, %01011101, %01011101, %01011100, %01011011, %01011011, %01011010, %01011010, %01011001, %01011001, %01011000, %01011000, %01010111, %01010110, %01010110, %01010101, %01010100, %01010100, %01010011, %01010010, %01010001, %01010001, %01010000, %01001111, %01001110, %01001101, %01001100, %01001010, %01001001, %01000111, %01000101, %01000000
+		.byte %01111101, %01111101, %01111101, %01111100, %01111011, %01111011, %01111010, %01111010, %01111001, %01111001, %01111000, %01111000, %01110111, %01110110, %01110110, %01110101, %01110100, %01110100, %01110011, %01110010, %01110001, %01110001, %01110000, %01101111, %01101110, %01101101, %01101100, %01101010, %01101001, %01100111, %01100101, %01100000
+		.byte %10011101, %10011101, %10011101, %10011100, %10011011, %10011011, %10011010, %10011010, %10011001, %10011001, %10011000, %10011000, %10010111, %10010110, %10010110, %10010101, %10010100, %10010100, %10010011, %10010010, %10010001, %10010001, %10010000, %10001111, %10001110, %10001101, %10001100, %10001010, %10001001, %10000111, %10000101, %10000000
+		.byte %10111101, %10111101, %10111101, %10111100, %10111011, %10111011, %10111010, %10111010, %10111001, %10111001, %10111000, %10111000, %10110111, %10110110, %10110110, %10110101, %10110100, %10110100, %10110011, %10110010, %10110001, %10110001, %10110000, %10101111, %10101110, %10101101, %10101100, %10101010, %10101001, %10100111, %10100101, %10100000
+		.byte %11011101, %11011101, %11011101, %11011100, %11011011, %11011011, %11011010, %11011010, %11011001, %11011001, %11011000, %11011000, %11010111, %11010110, %11010110, %11010101, %11010100, %11010100, %11010011, %11010010, %11010001, %11010001, %11010000, %11001111, %11001110, %11001101, %11001100, %11001010, %11001001, %11000111, %11000101, %11000000
+		.byte %11111101, %11111101, %11111101, %11111100, %11111011, %11111011, %11111010, %11111010, %11111001, %11111001, %11111000, %11111000, %11110111, %11110110, %11110110, %11110101, %11110100, %11110100, %11110011, %11110010, %11110001, %11110001, %11110000, %11101111, %11101110, %11101101, %11101100, %11101010, %11101001, %11100111, %11100101, %11100000
 
 ; upside down, to match the view buffer
 ; 22+ sets of 5 to match the 110 lines of frame buffer
@@ -1262,7 +1292,7 @@ perspectivetable
 
 level0
         ; platform start point in the level, platform end point, height of the platform, color (3 bits only, so only even numbers)
-        ; eg, this first one starts at 1, is 10 long, is 30 high, and points to the 1th entry in the colors table
+        ; eg, this first one starts at 1, is 10 long, is 30 high, and is green
 		dc.b 1, 11, $1e,  $e0
 		dc.b 20, 25, $14, $60
 		dc.b 30, 40, $18, $20
